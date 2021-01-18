13c6f3c8946b4bf70c15466906024bcb813c4333
Mario M.E.F. Elvira Ruiz <mario.elvira@ingeteam.com>
2021-01-18 08:22:47 +0100

    charger pin added

---
+++ _cargadorWeb/io.h
@@ -0,0 +1,49 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _IO_H_
+#define _IO_H_
+
+///////////////////
+// IO definition //
+///////////////////
+/* ----------------------------------
+ *   |                     --------
+ *   |__  |            RX -|7    8|- VCC
+ *    __| |         GPIO0 -|5    6|- RST
+ *   |__  |         GPIO2 -|3    4|- CH_PD
+ *    __| |           GND -|1*   2|- GPIO1 - TX
+ *   |____|                --------
+ *  ---------------------------------
+ */
+
+// PIN definition
+#define PIN_RELE_CORTE    1
+#define PIN_LED           2
+//#define PIN_LED_IND       4
+#define PIN_AUX1          3
+
+#define PIN_OUT_ON        1
+#define PIN_OUT_OFF       0
+
+#define PIN_IN_OFF        1
+
+// IO definition
+#define IN_ON             1
+#define IN_OFF            0
+
+#define OUT_ON            1
+#define OUT_OFF           0
+
+#define IN_STATUS_INIT    0
+#define IN_STATUS_ON      1
+#define IN_STATUS_OFF     2
+
+#define INDEX_AUX1        0
+
+#endif // _IO_H_
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ _cargadorWeb/io.ino
@@ -0,0 +1,60 @@
+
+//////////////////////
+// IO state machine //
+//////////////////////
+void _IOSetup(void)
+{
+  inState = IN_STATUS_INIT;
+  
+  outReleCorte = OUT_OFF;
+  outLedInd = OUT_OFF;
+}
+
+//////////////////////
+// IO state machine //
+//////////////////////
+void _IOLoop(void)
+{
+  switch (inState)
+  {
+	  case IN_STATUS_INIT:
+      inTimeTick = millis();
+      if (inValue == IN_ON)
+        inState = IN_STATUS_ON;
+      else
+        inState = IN_STATUS_OFF;
+
+      break;
+      
+	  case IN_STATUS_ON:
+      if (inValue == IN_OFF)
+      {
+        inState = IN_STATUS_INIT;
+        outReleCorte = OUT_OFF;
+        outLedInd = OUT_OFF;
+      }
+      else
+      {
+        if (millis() - inTimeTick >= (inTimeoutSec*1000))
+        {
+          // Rele de corte
+          outReleCorte = OUT_ON;
+
+          // Indicar Led
+          outLedInd = OUT_ON;
+        }
+      }
+      break;
+      
+	  case IN_STATUS_OFF:
+
+      outReleCorte = OUT_OFF;
+      outLedInd = OUT_OFF;
+   
+      inTimeTick = millis();
+      if (inValue == IN_ON)
+        inState = IN_STATUS_ON;
+
+	    break;
+  }
+}

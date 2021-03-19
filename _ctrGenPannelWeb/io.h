0faa5ed6e8251900ed95f80ee9ecd8609425f6db
Mario M.E.F. Elvira Ruiz <mario.elvira@ingeteam.com>
2021-03-19 21:03:45 +0100

    pin redefinition

--- _ctrGenPannelWeb/io.h
+++ _ctrGenPannelWeb/io.h
@@ -10,24 +10,25 @@ extern "C" {
 ///////////////////
 /*
  *                        --------
- *                    A0 -|      |- GPIO16 -> D0 -> GEN
- *                    G  -|      |- GPIO5  -> D1 -> DISP
- *                    VU -|      |- GPIO4  -> D2 -> ZUMB
- *         AS3 <- GPIO10 -|      |- GPIO0  -> D3 <- PULSADOR
- *         AS2 <-  GPIO9 -|      |- GPIO2  -> D4 -> LED*
+ *                    A0 -|      |- GPIO16 -> GEN
+ *                    G  -|      |- GPIO5  -> DISP
+ *                    VU -|      |- GPIO4  -> ZUMB
+ *         AS3 <- GPIO10 -|      |- GPIO0  <- PULSADOR
+ *         AS2 <-  GPIO9 -|      |- GPIO2  -> LED INTERNO*
  *          S1 <-   MOSI -|      |- 3V
  *                    CS -|      |- G
- *          S0 <-   MISO -|      |- GPIO14 -> D5 -> D3
- *          SK <-   SCLK -|      |- GPIO12 -> D6 -> D4
- *                     G -|      |- GPIO13 -> D7 -> D5
- *                    3V -|      |- GPIO15 -> D8 -> D6
- *                    EN -|      |- GPIO3  -> RX
- *                   RST -|      |- GPIO1  -> TX
+ *          S0 <-   MISO -|      |- GPIO14 -> D3
+ *          SK <-   SCLK -|      |- GPIO12 -> D4
+ *                     G -|      |- GPIO13 -> D5
+ *                    3V -|      |- GPIO15 -> D6
+ *                    EN -|      |- GPIO3 
+ *                   RST -|      |- GPIO1
  *                     G -|      |- G
  *                   VIN -|      |- 3V
  *                        --------
  */
 
+/*
 // PIN definition
 #define PIN_GEN           16
 #define PIN_DISP          5    
@@ -39,7 +40,40 @@ extern "C" {
 #define PIN_PULSADOR      0
 //#define PIN_LED_STATUS  9
 
-#define PIN_LED            2
+#define PIN_LED           2
+
+*/
+
+/*
+ *                        --------
+ *                    A0 -|      |- GPIO16 -> LED EXT
+ *                    G  -|      |- GPIO5  -> GEN
+ *                    VU -|      |- GPIO4  -> D3
+ *         AS3 <- GPIO10 -|      |- GPIO0  -> D4
+ *         AS2 <-  GPIO9 -|      |- GPIO2  -> D5
+ *          S1 <-   MOSI -|      |- 3V
+ *                    CS -|      |- G
+ *          S0 <-   MISO -|      |- GPIO14 -> D6
+ *          SK <-   SCLK -|      |- GPIO12 -> DISP
+ *                     G -|      |- GPIO13 -> ZUMB
+ *                    3V -|      |- GPIO15 <- PULSADOR
+ *                    EN -|      |- GPIO3  
+ *                   RST -|      |- GPIO1  
+ *                     G -|      |- G
+ *                   VIN -|      |- 3V
+ *                        --------
+ */
+
+#define PIN_GEN           5
+#define PIN_DISP          12   
+#define PIN_ZUMB          13
+#define PIN_D3            4
+#define PIN_D4            0
+#define PIN_D5            2
+#define PIN_D6            14
+#define PIN_PULSADOR      15
+
+#define PIN_LED           16
 
 #define PIN_OUT_ON        1
 #define PIN_OUT_OFF       0
